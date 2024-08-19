4b850ca60eb43aaaec28acc86397cef949419942
faizkasman97 <faizkasman@myra.com.my>
2024-08-19 18:17:03 +0800

    basic tw styling

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./public/**/*.html', // Adjust according to your file structure
+		'./public/js/**/*.js', // Adjust according to your file structure
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
